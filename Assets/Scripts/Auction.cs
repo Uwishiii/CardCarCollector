df5ce558c0efc3e7a5ef89cac96a0469cc9420fd
Uwishiii <dylanwever2002@gmail.com>
2023-03-25 15:09:21 +0100

    Better Click Detection System

--- Assets/Scripts/Auction.cs
+++ Assets/Scripts/Auction.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Auction : MonoBehaviour
 {
-    public List<GameObject> carList = new List<GameObject>();
-    public List<GameObject> carPosList = new List<GameObject>();
-    public List<GameObject> auctionCars = new List<GameObject>();
+    private List<GameObject> carList = new List<GameObject>();
+    private List<GameObject> carPosList = new List<GameObject>();
+    public List<GameObject> carBoughtPosList = new List<GameObject>();
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        InitiateLists();
+
+        CarPositioner();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CarSelector();
+    }
+
+    private void InitiateLists()
     {
         foreach (GameObject carPos in GameObject.FindGameObjectsWithTag("CarPos"))
         {
@@ -21,12 +36,18 @@ public class Auction : MonoBehaviour
         {
             carList.Add(car);
         }
+        
+        foreach (GameObject carBoughtPos in GameObject.FindGameObjectsWithTag("CarBoughtPos"))
+        {
+            carBoughtPosList.Add(carBoughtPos);
+        }
+    }
 
+    private void CarPositioner()
+    {
         for (int i = 0; i < 3; i++)
         {
             int randomCar = Random.Range(0, carList.Count);
-
-            //auctionCars.Add(carList[randomCar]);
 
             carList[randomCar].transform.position = carPosList[i].transform.position;
 
@@ -34,9 +55,20 @@ public class Auction : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CarSelector()
     {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                if (hit.transform != null) {
+                    //Debug.Log("Hit " + hit.transform.gameObject.name);
+                    
+                    int randomCarBoughtPos = Random.Range(0, carBoughtPosList.Count);
         
+                    hit.transform.position = carBoughtPosList[randomCarBoughtPos].transform.position;
+                    carBoughtPosList.Remove(carBoughtPosList[randomCarBoughtPos]);
+                }
+        }
     }
 }
